872082b4ee98d477fcacb742699a4981fddeb6c1
nicolasspaniol <nicolas.spaniol@institutoivoti.com.br>
2022-04-28 15:56:38 -0300

    aprimorado menu inicial

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -2,9 +2,22 @@ package view;
 
 import javax.swing.*;
 
-public class Menu {
-    private JTabbedPane tabbedPane1;
-    private JPanel panel1;
+public class Menu extends JFrame {
+    private JPanel mainPanel;
 
+    public Menu() {
+        setContentPane(mainPanel);
+        setVisible(true);
+        this.pack();
+    }
 
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        }
+        catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Erro: " + ex.getMessage());
+        }
+        Menu menu = new Menu();
+    }
 }
