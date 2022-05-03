0b09abdf896e351d7842c37f6c63cd0cdff05d13
nicolasspaniol <nicolas.spaniol@institutoivoti.com.br>
2022-05-03 17:04:30 -0300

    conserta a classe DAO e torna a anotacao CampoSQL opcional

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -3,9 +3,9 @@ package dao;
 import model.CampoSQL;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Objects;
 
 public abstract class DAO<T extends Record> {
@@ -20,7 +20,7 @@ public abstract class DAO<T extends Record> {
     public abstract void alterar(T obj) throws SQLException;
     public abstract void deletar(int id) throws SQLException;
 
-    public abstract Class<?> tipoRegistro();
+    abstract Class<?> tipoRegistro();
 
     public final String[] colunas() {
         Field[] campos = tipoRegistro().getDeclaredFields();
@@ -29,11 +29,7 @@ public abstract class DAO<T extends Record> {
         for (int i = 0; i < campos.length; i++) {
             Field campo = campos[i];
             CampoSQL an = campo.getAnnotation(CampoSQL.class);
-            if (an == null){
-                cols[i] = null;
-                continue;
-            }
-            String col = an.nomeColuna();
+            String col = an == null? "" : an.nomeColuna();
             if (Objects.equals(col, "")) {
                 col = campo.getName();
                 col = col.substring(0, 1).toUpperCase() + col.substring(1);
@@ -43,8 +39,11 @@ public abstract class DAO<T extends Record> {
 
         return cols;
     }
-    public final Object lerValor(Object obj, int col) throws IllegalAccessException {
-        return obj.getClass().getDeclaredFields()[col].get(obj);
+    public final Object lerValor(Object obj, int col) throws IllegalAccessException, InvocationTargetException {
+        return obj.getClass().getRecordComponents()[col].getAccessor().invoke(obj);
+    }
+    public final Class<?> lerClasse(int col) {
+        return tipoRegistro().getRecordComponents()[col].getType();
     }
 
     public static String[] tabelas = new String[] {"Municipio", "Categoria de reporte"};
--- src/model/Municipio.java
+++ src/model/Municipio.java
@@ -5,8 +5,6 @@ import util.Estado;
 public record Municipio (
         @CampoSQL(nomeColuna = "ID")
         int idMunicipio,
-        @CampoSQL
         String nome,
-        @CampoSQL
         Estado estado
 ) {}
